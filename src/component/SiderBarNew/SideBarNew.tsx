f99298d0abd64f9307923ca6de29b6a7a7b259fb
Oleg Tkach <KratosHome101@gmail.com>
2022-07-29 14:03:35 +0300

    created pagesSideBar

--- src/component/SiderBarNew/SideBarNew.tsx
+++ src/component/SiderBarNew/SideBarNew.tsx
@@ -13,12 +13,12 @@ export const SideBarNew: FC<SideBarNewType> = memo(({setCurrentChannel}) => {
         <div className="containerSideBar">
             <div className="headSideBar">
                 <Logo/>
-                <div className="brend">
+                <div className="brandSideBarNew">
                     <span>Company Name</span>
-                        <svg data-0v2="true" aria-hidden="true" viewBox="0 0 20 20" className="">
-                            <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                  stroke-width="1.5" d="M13.75 8 10 11.75 6.25 8"></path>
-                        </svg>
+                    <svg data-0v2="true" aria-hidden="true" viewBox="0 0 20 20" className="">
+                        <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                              stroke-width="1.5" d="M13.75 8 10 11.75 6.25 8"></path>
+                    </svg>
                 </div>
                 <div>
                     <svg data-0v2="true" aria-hidden="true" viewBox="0 0 20 20" className="">
@@ -32,20 +32,60 @@ export const SideBarNew: FC<SideBarNewType> = memo(({setCurrentChannel}) => {
                     </svg>
                 </div>
             </div>
-            <div>
+            <div className="pagesSideBar">
                 <div>
+                    <svg data-0v2="true" aria-hidden="true" viewBox="0 0 20 20">
+                        <g fill="none" stroke="currentColor" stroke-width="1.5">
+                            <path stroke-linejoin="round"
+                                  d="M16.78 13.759a7.75 7.75 0 1 0-3.02 3.02l3.99.971-.97-3.991Z"></path>
+                            <path stroke-linecap="round" d="M6.75 8.25h6.5m-6.5 3.5h4.5"></path>
+                        </g>
+                    </svg>
                     <div>Threads</div>
                 </div>
                 <div>
+                    <svg data-0v2="true" aria-hidden="true" viewBox="0 0 20 20">
+                        <g fill="none" stroke="currentColor" stroke-width="1.5">
+                            <circle cx="10" cy="10" r="3.75"></circle>
+                            <path stroke-linecap="round"
+                                  d="M13.75 6.75v4.605c0 1.047.848 1.895 1.895 1.895v0a2.605 2.605 0 0 0 2.605-2.605V10"></path>
+                            <path stroke-linecap="round" d="M18.25 10a8.25 8.25 0 1 0-3.497 6.744"></path>
+                        </g>
+                    </svg>
                     <div>Mentions & reactions</div>
                 </div>
                 <div>
+                    <svg data-0v2="true" aria-hidden="true" viewBox="0 0 20 20">
+                        <path fill="none" stroke="currentColor" stroke-width="1.5"
+                              d="M3.25 5.25a3 3 0 0 1 3-3h7.5a3 3 0 0 1 3 3v11.642c0 .766-.927 1.15-1.469.607l-5.21-5.218a.1.1 0 0 0-.142 0l-5.21 5.218c-.542.543-1.469.16-1.469-.607V5.25Z"></path>
+                    </svg>
                     <div>Saved items</div>
                 </div>
                 <div>
+                    <svg data-0v2="true" aria-hidden="true" viewBox="0 0 20 20">
+                        <g fill="none">
+                            <path stroke="currentColor" stroke-width="1.5"
+                                  d="M1.75 4.75a3 3 0 0 1 3-3h5.5a3 3 0 0 1 3 3v13.5H2.75a1 1 0 0 1-1-1V4.75Z"></path>
+                            <path stroke="currentColor" stroke-width="1.5"
+                                  d="M13.5 8h1.75a3 3 0 0 1 3 3v6.25a1 1 0 0 1-1 1H13M9.75 18v-2.25a1 1 0 0 0-1-1h-2.5a1 1 0 0 0-1 1V18"></path>
+                            <circle cx="6" cy="8.5" r="1" fill="currentColor"></circle>
+                            <circle cx="6" cy="5.5" r="1" fill="currentColor"></circle>
+                            <circle cx="9" cy="5.5" r="1" fill="currentColor"></circle>
+                            <circle cx="9" cy="8.5" r="1" fill="currentColor"></circle>
+                            <circle cx="9" cy="11.5" r="1" fill="currentColor"></circle>
+                            <circle cx="6" cy="11.5" r="1" fill="currentColor"></circle>
+                        </g>
+                    </svg>
                     <div>Slack Connect</div>
                 </div>
                 <div>
+                    <svg data-0v2="true" aria-hidden="true" viewBox="0 0 20 20">
+                        <g fill="currentColor">
+                            <circle cx="10" cy="3.75" r="1.75" transform="rotate(90 10 3.75)"></circle>
+                            <circle cx="10" cy="10" r="1.75" transform="rotate(90 10 10)"></circle>
+                            <circle cx="10" cy="16.25" r="1.75" transform="rotate(90 10 16.25)"></circle>
+                        </g>
+                    </svg>
                     <div>More</div>
                 </div>
             </div>
--- src/component/messages/MessageHeader.tsx
+++ src/component/messages/MessageHeader.tsx
@@ -1,42 +1,43 @@
-import React, { useState } from 'react';
-import { BsPeopleFill } from 'react-icons/bs';
-import { channelParticipants } from '../../services/channelParticipants.service'
-import { participants } from '../../services/participant.service'
-import { Tooltip } from '../shared/Tooltip'
+import React, {useState} from 'react';
+import {BsPeopleFill} from 'react-icons/bs';
+import {channelParticipants} from '../../services/channelParticipants.service'
+import {participants} from '../../services/participant.service'
+import {Tooltip} from '../shared/Tooltip'
 
 interface iMessageHeaderProps {
-    currentChannel:string,
+    currentChannel: string,
     currentChannelId: number,
 }
+
 export const MessageHeader: React.FC<iMessageHeaderProps> = ({currentChannel, currentChannelId}) => {
-const participansArr: any = []
-const [show, setShow] = useState(false)
-const channelParticipantsId = channelParticipants.find(el => el.channelId === currentChannelId)
-const participantsId = channelParticipantsId?.participantsId
-participantsId?.map(el => {
-    participansArr?.push(participants?.find(e => e.id === el)?.name);
-})
-const participantsCount = participantsId ? participantsId.length : 0;
-  return(
-      <div className="msgs_header">
-      <div className="super_wrapper">
-    <div className="curr_ch_wrapper">
-      <h3 className='current_channel'>#{currentChannel}</h3>
-    </div>
-          <div className="intermediary">
-          </div>
+    const participansArr: any = []
+    const [show, setShow] = useState(false)
+    const channelParticipantsId = channelParticipants.find(el => el.channelId === currentChannelId)
+    const participantsId = channelParticipantsId?.participantsId
+    participantsId?.map(el => {
+        participansArr?.push(participants?.find(e => e.id === el)?.name);
+    })
+    const participantsCount = participantsId ? participantsId.length : 0;
+    return (
+        <div className="msgs_header">
+            <div className="super_wrapper">
+                <div className="curr_ch_wrapper">
+                    <h3 className='current_channel'>#{currentChannel}</h3>
+                </div>
+                <div className="intermediary">
+                </div>
 
-        <div className="people_wrapper">
-        <div className="people">
-            <div className="people-round">
-        <BsPeopleFill onMouseOver={() => setShow(true)} onMouseOut={()=> setShow(false)} />
-            <div className="participants_count">{participantsCount}</div>
+                <div className="people_wrapper">
+                    <div className="people">
+                        <div className="people-round">
+                            <BsPeopleFill onMouseOver={() => setShow(true)} onMouseOut={() => setShow(false)}/>
+                            <div className="participants_count">{participantsCount}</div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
-            </div>
+            <Tooltip users={participansArr} show={show} currentChannelId={currentChannelId}/>
         </div>
-
-      </div>
-          <Tooltip users={participansArr} show={show} currentChannelId={currentChannelId}/>
-      </div>
-  )
+    )
 } 
