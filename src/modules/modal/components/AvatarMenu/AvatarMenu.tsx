d71eeec01f5dac060255e66fdd529d721e43110c
VladyslavLohynskyi <watcher.good2003@gmail.com>
2022-08-26 19:28:08 +0300

    added an additional popup for avatar menu

--- src/modules/modal/components/AvatarMenu/AvatarMenu.tsx
+++ src/modules/modal/components/AvatarMenu/AvatarMenu.tsx
@@ -1,28 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MenuItem } from '../MenuItem';
 import { MenuSeparatorItem } from '../MenuSeparatorItem';
 import { MenuUserItem } from '../MenuUserItem';
 import { MenuUserStatusItem } from '../MenuUserStatusItem';
+import { PauseNotifications } from './PauseNotifications';
 import { AvatarMenuPropsType } from './AvatarMenuType';
-
+import "./AvatarMenu.scss"
 export const AvatarMenu: React.FC<AvatarMenuPropsType> = ({
    currentUserName,
 }) => {
+   const [isModalPauseNotifications, setIsModalPauseNotifications] = useState(false);
    return (
-      <div className='container-menu'>
-         <div className='container-menu__items'>
-            <MenuUserItem currentUserName={currentUserName} />
-            <MenuUserStatusItem />
-            <MenuItem itemText={'Set yourself as away'} />
-            <div className='container-menu__submenu'>
-               <MenuItem isSubmenu={true} itemText={'Pause notifications'} />
+      <>
+         <div className='container-menu'>
+            <div className='container-menu__items'>
+               <MenuUserItem currentUserName={currentUserName} />
+               <MenuUserStatusItem />
+               <MenuItem itemText={'Set yourself as away'} />
+               <div 
+                  className={`container-menu__submenu ${isModalPauseNotifications?"avatar-menu__popup-active":""}` }
+                  onMouseOver={()=>setIsModalPauseNotifications(true)} 
+                  onMouseOut={()=>setIsModalPauseNotifications(false)}>
+                  <MenuItem isSubmenu={true} itemText={'Pause notifications'} />
+               </div>
+               <MenuSeparatorItem />
+               <MenuItem itemText={'Profile'} />
+               <MenuItem itemText={'Preferences'} />
+               <MenuSeparatorItem />
+               <MenuItem itemText={'Sign out of Company Name'} />
             </div>
-            <MenuSeparatorItem />
-            <MenuItem itemText={'Profile'} />
-            <MenuItem itemText={'Preferences'} />
-            <MenuSeparatorItem />
-            <MenuItem itemText={'Sign out of Company Name'} />
          </div>
-      </div>
+         {isModalPauseNotifications?
+            <PauseNotifications onMouseOver={()=>setIsModalPauseNotifications(true)} onMouseOut={()=>setIsModalPauseNotifications(false)} />:null}
+         
+      </>
    );
 };
